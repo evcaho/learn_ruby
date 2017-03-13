5610404e77663662586848bcf6e0471245b764d1
Evelyn Caldwell-Horne <evcaho@Evelyns-Air.domain>
2017-03-13 15:03:59 -0700

    initial exercises

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,14 @@
 #write your code here
+def add (param1, param2)
+	param1 + param2
+end
+
+def subtract(param1, param2)
+	param1 - param2
+end
+
+def sum(params)
+	total = 0
+	params.inject(0, &:+)
+end
+
