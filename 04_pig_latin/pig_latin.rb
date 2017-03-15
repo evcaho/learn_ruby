2fd33f65baca3c5f2645b2d3b02f16075ebaa981
Evelyn Caldwell-Horne <evcaho@Evelyns-Air.domain>
2017-03-15 14:29:32 -0700

    needs refactoring

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -5,12 +5,22 @@ def translate(words)
 		first = word[0]
 		second = word[1]
 		third = word[2]
+		qu = "qu"
 		if vowels.include?(first)
 			"#{word}ay"
+		elsif vowels.include?(first) == false && vowels.include?(second) == false && vowels.include?(third) == false
+			word.tr!(first, "") 
+			word.tr!(second, "")
+			word.tr!(third, "")
+			"#{word}#{first}#{second}#{third}ay"
 		elsif vowels.include?(first) == false && vowels.include?(second) == false
 			word.tr!(first, "") 
 			word.tr!(second, "")
 			"#{word}#{first}#{second}ay"
+		elsif word[0] == "q" && word[1] == "u"
+			word.tr!(first, "")
+			word.tr!(second, "")
+			"#{word}#{qu}ay"
 		else
 			word.tr!(first, "")
 			"#{word}#{first}ay"
