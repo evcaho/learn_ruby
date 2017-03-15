ea771125653f359f5bc4477647b2e7fbf36111f5
Evelyn Caldwell-Horne <evcaho@Evelyns-Air.domain>
2017-03-15 14:11:32 -0700

    saving wip - needs refactoring

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,21 @@
-#write your code here
+def translate(words)
+	vowels = ["a", "e", "i", "o", "u"]
+	word_string = words.split(" ")
+	word_string.map! do |word|
+		first = word[0]
+		second = word[1]
+		third = word[2]
+		if vowels.include?(first)
+			"#{word}ay"
+		elsif vowels.include?(first) == false && vowels.include?(second) == false
+			word.tr!(first, "") 
+			word.tr!(second, "")
+			"#{word}#{first}#{second}ay"
+		else
+			word.tr!(first, "")
+			"#{word}#{first}ay"
+		end	
+	end
+	word_string.join(" ")
+
+end
