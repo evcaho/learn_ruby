fe4e5cfd4cbfd74babc5b68cc75bfeee87fec641
Evelyn Caldwell-Horne <evcaho@Evelyns-Air.domain>
2017-03-20 14:48:18 -0700

    passed

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,23 @@
 class Book
-# write your code here
+	attr_writer :title
+	def title
+		@title.capitalize
+		words_no_cap = ["and", "or", "the", "over", "to", "the", "a", "but", "in", "an", "of"]
+   		phrase = @title.split(" ")
+   		first = 0
+   		phrase.map do |word|
+       		if words_no_cap.include?(word) && first != 0
+       			first += 1
+           		word
+       		 else
+       		 	first += 1
+           		word.capitalize
+        	end
+  	 	 end.join(" ")
+	end
+
 end
+
+
+
+
