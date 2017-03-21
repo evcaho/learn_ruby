23ea1646570ee448c0555f3ced81e94fab1e86f9
Evelyn Caldwell-Horne <evcaho@Evelyns-Air.domain>
2017-03-21 15:42:24 -0700

    finished timer, happy w code

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,23 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+  	@seconds = 0
+  end
+
+  def time_string
+  		hours = (@seconds/3600)
+  		if hours < 10
+  			hours = "0#{hours}"
+  		end
+  		minutes = (@seconds%3600)/60
+  		if minutes < 10
+  			minutes = "0#{minutes}"
+  		end
+  		seconds = (@seconds % 3600)%60
+  		if seconds < 10
+  			seconds = "0#{seconds}"
+  		end
+  		"#{hours}:#{minutes}:#{seconds}"
+  end
 end
